ab9f9b6cd02dcf67c23f49fdd82293b1f6d8b3ed
Ange-BC <andriniaina.randria@gmail.com>
2023-10-02 08:47:12 +0200

    crud shadcn

---
+++ react-crud/src/pages/user/faker.ts
@@ -0,0 +1,128 @@
+const users = [
+  {
+    lastname: "Smith",
+    firstname: "John",
+    email: "john.smith@example.com",
+    _id: "123456789",
+    address: "123 Main St",
+    city: "New York",
+    zipCode: "12345",
+    country: "USA",
+    phone: "123-456-7890",
+    status: "active"
+  },
+  {
+    lastname: "Johnson",
+    firstname: "Sarah",
+    email: "sarah.johnson@example.com",
+    _id: "987654321",
+    address: "456 Elm St",
+    city: "Los Angeles",
+    zipCode: "98765",
+    country: "USA",
+    phone: "987-654-3210",
+    status: "inactive"
+  },
+  {
+    lastname: "Brown",
+    firstname: "Michael",
+    email: "michael.brown@example.com",
+    _id: "456789123",
+    address: "789 Oak St",
+    city: "Chicago",
+    zipCode: "45678",
+    country: "USA",
+    phone: "456-789-1230",
+    status: "active"
+  },
+  {
+    lastname: "Davis",
+    firstname: "Jennifer",
+    email: "jennifer.davis@example.com",
+    _id: "789123456",
+    address: "321 Pine St",
+    city: "Houston",
+    zipCode: "78901",
+    country: "USA",
+    phone: "789-123-4560",
+    status: "inactive"
+  },
+  {
+    lastname: "Wilson",
+    firstname: "Robert",
+    email: "robert.wilson@example.com",
+    _id: "654321987",
+    address: "654 Maple St",
+    city: "Phoenix",
+    zipCode: "65432",
+    country: "USA",
+    phone: "654-321-9870",
+    status: "active"
+  },
+  {
+    lastname: "Martinez",
+    firstname: "Maria",
+    email: "maria.martinez@example.com",
+    _id: "321789456",
+    address: "987 Cedar St",
+    city: "San Francisco",
+    zipCode: "32198",
+    country: "USA",
+    phone: "321-789-4560",
+    status: "inactive"
+  },
+  {
+    lastname: "Anderson",
+    firstname: "David",
+    email: "david.anderson@example.com",
+    _id: "987456123",
+    address: "456 Birch St",
+    city: "Seattle",
+    zipCode: "98745",
+    country: "USA",
+    phone: "987-456-1230",
+    status: "active"
+  },
+  {
+    lastname: "Taylor",
+    firstname: "Jessica",
+    email: "jessica.taylor@example.com",
+    _id: "456123789",
+    address: "789 Walnut St",
+    city: "Miami",
+    zipCode: "45612",
+    country: "USA",
+    phone: "456-123-7890",
+    status: "inactive"
+  },
+  {
+    lastname: "Miller",
+    firstname: "James",
+    email: "james.miller@example.com",
+    _id: "789456321",
+    address: "123 Spruce St",
+    city: "Dallas",
+    zipCode: "78945",
+    country: "USA",
+    phone: "789-456-3210",
+    status: "active"
+  },
+  {
+    lastname: "Thomas",
+    firstname: "Emily",
+    email: "emily.thomas@example.com",
+    _id: "321456789",
+    address: "321 Oak St",
+    city: "Atlanta",
+    zipCode: "32145",
+    country: "USA",
+    phone: "321-456-7890",
+    status: "inactive"
+  }
+];
+const fakeUsers = {
+  response: 200,
+  datas: users
+};
+
+export { fakeUsers };
